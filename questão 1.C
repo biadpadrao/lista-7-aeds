e231d04030252f77234203fd3e47626ce49fafd4
Beatriz Padrão <103266259+biadpadrao@users.noreply.github.com>
2022-05-26 15:15:31 -0300

    Create questão 1.C

---
+++ questão 1.C
@@ -0,0 +1,42 @@
+/* Dados os seguintes campos de um registro: nome, dia de aniversário e mês de aniversário,
+desenvolver um algoritmo que mostre em cada um dos meses do ano quem são as pessoas que fazem aniversário,
+exibir também o dia. Considere um conjunto de 40 pessoas. */
+
+#include <stdio.h>
+#include <string.h>
+
+typedef struct Registro {
+  int dia;
+  int mes;
+} Registro;
+
+typedef struct Pessoa {
+  char nome [100];
+  Registro dtanasc;
+} Pessoa;
+
+int main(void) {
+  int n = 5;
+  Pessoa p[n] ;
+  for (int i=0;i<n;i++){
+    printf("\nDigite seu nome: ");
+    scanf("%s", p[i].nome);
+    printf("Digite seu dia de nascimento: ");
+    scanf("%d", &p[i].dtanasc.dia);
+    printf("Digite seu mês de nascimento: ");
+    scanf("%d", &p[i].dtanasc.mes);
+    getchar();
+  }
+  int achou=0;
+  for (int m=1;m<=12;m++){
+    printf("\nAniversariante do mês %d: \n", m);
+    for (int i=0;i<n;i++){
+      if (m == p[i].dtanasc.mes) {
+        printf("%s \n", p[i].nome);
+        achou = 1;
+      }
+    }
+    if (achou ==0 )
+      printf("Sem aniversariantes \n");
+    achou = 0;
+  } 
